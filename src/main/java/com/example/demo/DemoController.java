4a2f44217990acf4b454e30d8e5423303a830207
wsp4 <vinojjohn@yahoo.co.in>
2024-06-08 12:47:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/message")
+    public String message(){
+        return "Application deployed to Azure";
+    }
+}
