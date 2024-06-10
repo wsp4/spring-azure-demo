b5bc4037484162d2d6f0aeb38bb107e416b2ad15
wsp4 <vinojjohn@yahoo.co.in>
2024-06-10 16:02:08 +0530

    second commit

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -8,6 +8,6 @@ public class DemoController {
 
     @GetMapping("/message")
     public String message(){
-        return "Application deployed to Azure";
+            return "Application deployed to Azure and set for continuous deployment";
     }
 }
